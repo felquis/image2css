90159061771dbc77be7c802e5d0f087ecf034816
Felquis Gimenes <frgformenton@gmail.com>
2014-08-07 15:12:41 -0300

    Add choose files and drag'n drop files

--- script.js
+++ script.js
@@ -1,72 +1,56 @@
-var image = new Image();
-var imageData;
-var x, y;
-var canvas = document.createElement('canvas');
-var ctx;
-var canvasHeight;
-var canvasWidth;
-var data;
-var ArrayBoxshadow = [];
-var alpha;
+function handleFileSelect(evt) {
 
-image.src = 'gun.png';
+  evt.stopPropagation();
+  evt.preventDefault();
 
-var suffix = function (number) {
-  if (number > 0) {
-    return number + 'em';
-  }
+  var files = evt.target.files || (evt.dataTransfer || {}).files;
 
-  return number;
-}
+  for (var i = 0, file; file = files[i]; i++) {
 
-var RGBToHex = function (r, g, b) {
-  function componentToHex(c) {
-    var hex = c.toString(16);
-    return hex.length == 1 ? "0" + hex : hex;
-  }
-
-  return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
-}
-
-var imageLoaded = function (event) {
-
-  var image = event.currentTarget;
-
-  // dimensões da imagem
-  canvas.height = image.height;
-  canvas.width = image.width;
-
-  canvasHeight = canvas.height;
-  canvasWidth = canvas.width;
-
-  ctx = canvas.getContext('2d');
-
-  // Desenha imagem no canvas
-  ctx.drawImage(image, 0, 0);
-
-  // retorna array de pixels
-  data = ctx.getImageData(0, 0, canvasWidth, canvasHeight);
-
-  for(var i = 0; i < data.data.length; i += 4) {
-    x = (i / 4) % canvasWidth;
-    y = Math.floor((i / 4) / canvasWidth);
-    alpha = data.data[i + 3] / 255;
-
-    if (alpha < 1 && alpha < 0) {
-      ArrayBoxshadow.push(suffix(x) + ' ' + suffix(y) + ' rgba('+ data.data[i] +', '+ data.data[i + 1] +', '+ data.data[i + 2] +', '+ alpha +')');
-    } else if (alpha === 1) {
-      ArrayBoxshadow.push(suffix(x) + ' ' + suffix(y) + ' ' + RGBToHex(data.data[i], data.data[i + 1], data.data[i + 2]));
+    // Only process image files.
+    if (!file.type.match('image.*')) {
+      continue;
     }
+
+    var reader = new FileReader();
+
+    reader.readAsDataURL(file);
+
+    reader.onload = (function(theFile) {
+      return function(e) {
+
+        var span = document.createElement('span');
+        span.classList.add('item');
+
+        span.innerHTML = ['<img class="thumb" src="', e.target.result,
+                          '" title="', escape(theFile.name), '"/>'].join('');
+
+        image2css({
+          images: [e.target.result]
+        }, function (images) {
+          images.forEach(function (image) {
+            var i = document.createElement('i');
+
+            i.style.boxShadow = image.boxshadow;
+
+            span.appendChild(i);
+
+            document.querySelector('.list').insertBefore(span, null);
+          })
+        })
+      };
+    })(file);
   }
-
-  ctx.putImageData(data, 0, 0);
-
-  document.querySelector('body').appendChild(canvas);
-
-  var i = document.createElement('i');
-  i.style.boxShadow = ArrayBoxshadow.join(',');
-
-  document.querySelector('body').appendChild(i);
 }
 
-image.onload = imageLoaded;
+function handleDragOver(evt) {
+  evt.stopPropagation();
+  evt.preventDefault();
+  evt.dataTransfer.dropEffect = 'copy';
+}
+
+document.getElementById('files').addEventListener('change', handleFileSelect, false);
+
+var dropZone = document.querySelector('.drop-zone');
+dropZone.addEventListener('dragover', handleDragOver, false);
+dropZone.addEventListener('drop', handleFileSelect, false);
